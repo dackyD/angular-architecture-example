adb1b2426c83202a378bbff80ba1d9df1411f290
Harvey Daclan <hd@effera.no>
2023-12-18 12:29:34 +0100

    - add home and admin lazy features pp.48 - pp.51

--- projects/example-app/src/app/app.routes.ts
+++ projects/example-app/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./features/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'admin',
+    loadComponent: () =>
+      import('./features/admin/admin.component').then((m) => m.AdminComponent),
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
+];
