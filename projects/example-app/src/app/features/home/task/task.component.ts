530d5e7bd5672b8ff9d4325712bb5358536204f8
Harvey Daclan <hd@effera.no>
2024-01-05 11:42:19 +0100

    another way to lazy load components used in a modal

--- projects/example-app/src/app/features/home/task/task.component.ts
+++ projects/example-app/src/app/features/home/task/task.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { defer } from 'rxjs';
 
 @Component({
   selector: 'my-org-task',
@@ -11,10 +12,21 @@ import { MatDialog } from '@angular/material/dialog';
 })
 export class TaskComponent {
   private readonly dialog = inject(MatDialog);
+  private readonly taskModal$ = defer(() =>
+    import('./task-modal/task-modal.component').then(
+      ({ TaskModalComponent }) => TaskModalComponent,
+    ),
+  );
   async createTagModal() {
     const { TaskModalComponent } = await import(
       './task-modal/task-modal.component'
     );
     this.dialog.open(TaskModalComponent);
+  }
+
+  createTagModal2() {
+    this.taskModal$.subscribe((TaskModalComponent) => {
+      this.dialog.open(TaskModalComponent);
+    });
   }
 }
